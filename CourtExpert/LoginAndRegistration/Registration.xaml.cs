cea420e06bdc5995e998468e8ccfe480121318b2
Ignat Darin <ignat.darin@mail.ru>
2021-02-17 17:51:59 +0500

    Небольшие исправления

--- CourtExpert/LoginAndRegistration/Registration.xaml.cs
+++ CourtExpert/LoginAndRegistration/Registration.xaml.cs
@@ -23,5 +23,10 @@ namespace CourtExpert
         {
             InitializeComponent();
         }
+
+        private void ButtonLog_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
